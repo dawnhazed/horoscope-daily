7bd1d7386689f0ac9e0f769ecec811201a78f396
nadaa shafa <137472619+dawnhazed@users.noreply.github.com>
2025-12-22 17:28:41 +0700

    feat: local notification setup and horoscope model

---
+++ horoscope daily/Model/Horoscope.swift
@@ -0,0 +1,13 @@
+//
+//  Horoscope.swift
+//  horoscope daily
+//
+//  Created by Nadaa Shafa Nadhifa on 22/12/25.
+//
+
+import SwiftUI
+
+struct Horoscope: Codable, Hashable {
+    let date: String
+    let horoscope_data: String
+}
---
+++ horoscope daily/View/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  horoscope daily
+//
+//  Created by Nadaa Shafa Nadhifa on 22/12/25.
+//
+
+import SwiftUI
+import Foundation
+import SwiftData
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button ("Show Notification") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                    
+                    if granted {
+                        let content = UNMutableNotificationContent()
+                        content.title = "how's your day?"
+                        content.body = "your daily horoscope is here. check it out!"
+                        
+                        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                        // choose a random identifier
+                        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                        // add our notification request
+                        UNUserNotificationCenter.current().add(request)
+                    }
+                }
+            }
+            .padding()
+            
+            Text ("daily horoscope lays here")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        //.modelContainer(for: Item.self, inMemory: true)
+}
