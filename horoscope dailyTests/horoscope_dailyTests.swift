90800091c55d61ef7f261f912bad3390a92bf0ba
nadaa shafa <137472619+dawnhazed@users.noreply.github.com>
2025-12-22 16:31:09 +0700

    Initial Commit

---
+++ horoscope dailyTests/horoscope_dailyTests.swift
@@ -0,0 +1,17 @@
+//
+//  horoscope_dailyTests.swift
+//  horoscope dailyTests
+//
+//  Created by Nadaa Shafa Nadhifa on 22/12/25.
+//
+
+import Testing
+@testable import horoscope_daily
+
+struct horoscope_dailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
