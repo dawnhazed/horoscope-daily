b5a2a7fc8810c89653504f83fdff810039d0fd6c
nadaa shafa <137472619+dawnhazed@users.noreply.github.com>
2026-01-28 16:05:38 +0700

    feat: user info circles

--- horoscope daily/Model/Profile.swift
+++ horoscope daily/Model/Profile.swift
@@ -12,4 +12,29 @@ struct Profile: Codable, Hashable {
     let birthdate: Birthdate
     let zodiac: String
     let isBirthdateDone: Bool
+    
+    var zodiacEnum: Zodiac? {
+        Zodiac(rawValue: zodiac.lowercased())
+    }
+}
+
+enum Zodiac: String, Codable, CaseIterable {
+    case capricorn, aquarius, aries, pisces, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius
+    
+    var icon : String {
+        switch self {
+            case .capricorn: return "♑️"
+            case .aquarius: return "♒️"
+            case .aries: return "♈️"
+            case .pisces : return "♓️"
+            case .taurus: return "♉️"
+            case .gemini: return "♊️"
+            case .cancer: return "♋️"
+            case .leo: return "♌️"
+            case .virgo: return "♍️"
+            case .libra: return "♎️"
+            case .scorpio: return "♏️"
+            case .sagittarius: return "♐️"
+        }
+    }
 }
--- horoscope daily/View/ProfileView.swift
+++ horoscope daily/View/ProfileView.swift
@@ -11,22 +11,92 @@ import Combine
 struct ProfileView: View {
     
     @ObservedObject var profileVM: ProfileViewModel
+    
     let profile: Profile?
     
     var body: some View {
-        ZStack {
-            LinearGradient(stops: [
-                Gradient.Stop(color: .hdPink, location: 0.0),
-                Gradient.Stop(color: .hdPurple, location: 0.32),
-                Gradient.Stop(color: .hdViolet, location: 1.0)
-            ], startPoint: .topTrailing, endPoint: .bottomLeading)
+
+        GeometryReader { geo in
             
-            if let birthdate = profile?.birthdate {
-                Text("Birthdate: \(birthdate)")
-            } else {
-                Text("Birthdate: -")
+            let center = CGPoint(
+                x: geo.size.width / 2,
+                y: geo.size.height / 2
+            )
+            
+            ZStack {
+                LinearGradient(stops: [
+                    Gradient.Stop(color: .hdPink, location: 0.0),
+                    Gradient.Stop(color: .hdPurple, location: 0.32),
+                    Gradient.Stop(color: .hdViolet, location: 1.0)
+                ], startPoint: .topTrailing, endPoint: .bottomLeading)
+                
+                ForEach(0..<5) { i in
+                    
+                    let widthheight: CGFloat = 100 + ((CGFloat(i) + 1) * 100)
+                    let opacity: Double = 1.0 - (Double(i) * 0.2)
+                    
+                    Circle()
+                        .stroke(Color.white, lineWidth: 1).opacity(opacity)
+                        .frame(width: widthheight, height: widthheight)
+                    
+                    if i == 1 {
+                        Circle()
+                            .stroke(Color.white, lineWidth: 1).opacity(opacity)
+                            .frame(width: widthheight, height: widthheight)
+                    } else if i == 2 {
+                        
+                    }
+                    
+                }
+                
+                if let zodiac = profile?.zodiacEnum?.icon {
+                    CircleItem(
+                        circleColor: .white,
+                        icon: zodiac,
+                        radius: 150,
+                        angle: 210,
+                        diameter: 67                    )
+                }
+                
+                if let birthdate = profile?.birthdate {
+                    
+                    var birthdateDay: String {
+                        if birthdate.day < 10 {
+                            return "0\(birthdate.day)"
+                        } else {
+                            return "\(birthdate.day)"
+                        }
+                    }
+                    
+                    CircleItem(
+                        circleColor: .hdGreen,
+                        textColor: .black,
+                        text: "\(birthdateDay)/\(birthdate.month)",
+                        radius: 200,
+                        angle: 310,
+                        diameter: 81
+                    )
+                    
+                    CircleItem(
+                        circleColor: .hdPink,
+                        textColor: .white,
+                        text: "'\(String(format: "%02d", birthdate.year % 100))",
+                        radius: 150,
+                        angle: 45,
+                        diameter: 61
+                    )
+                }
+                
+                
+//                if let birthdate = profile?.birthdate {
+//                    Text("Birthdate: \(birthdate.day)")
+//                } else {
+//                    Text("Birthdate: -")
+//                }
             }
+            .ignoresSafeArea()
+            .position(center)
         }
-        .ignoresSafeArea()
     }
 }
+
